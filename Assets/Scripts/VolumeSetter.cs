7621f534900ac8483200b0b2687908fa61cf5df8
Unknown <dartic.axj@gmail.com>
2019-01-14 20:11:51 +0100

    YES C'EST PAS CASSE BEST DAY EVER

--- Assets/Scripts/VolumeSetter.cs
+++ Assets/Scripts/VolumeSetter.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 [RequireComponent(typeof (AudioSource))]
 public class VolumeSetter : MonoBehaviour {
-
+    public float multiplier = 1f;
 	// Use this for initialization
 	void Start () {
         if (Gamemanager.gamemanager != null)
-        GetComponent<AudioSource>().volume = Gamemanager.gamemanager.volume;
+        GetComponent<AudioSource>().volume = (Gamemanager.gamemanager.volume * multiplier);
 	}
 	
 	// Update is called once per frame
